f0e1ac95ce22bfabd5d0455a743a734499d02b59
walzer85 <walzer85@gmail.com>
2018-05-21 22:21:05 -0500

    got to the first product supercomponent

--- src/App.js
+++ src/App.js
@@ -16,21 +16,21 @@ function App(props) {
       {/* send in a prop called "message", give it the string "Hello World" */}
       <div>This better say "Hello World": <SuperComponent message={"Hello World"} /> </div>
       {/* send in a prop called "message", give it the string "Goodbye World" */}
-      <div>This better say "Goodbye World": <SuperComponent /> </div>
+      <div>This better say "Goodbye World": <SuperComponent message={"Goodbye World"} /> </div>
       {/* send in a prop called "message", give it the string "Props are awesome" */}
-      <div>This better say "Props are awesome": <SuperComponent /> </div>
+      <div>This better say "Props are awesome": <SuperComponent message={"Props are awesome"} /> </div>
       {/* send in a prop called "message", give it the string "I totally get this now" */}
-      <div>This better say "I totally get this now": <SuperComponent /> </div>
+      <div>This better say "I totally get this now": <SuperComponent message={"I totally get this now"} /> </div>
 
 
       {/* send in a prop called "magicNumber", give it the number 42*/}
-      <div>This better say 42: <SuperComponent /> </div>
+      <div>This better say 42: <SuperComponent magicNumber={42}/> </div>
       {/* send in a prop called "magicNumber", give it the number 21*/}
-      <div>This better say 21: <SuperComponent /> </div>
+      <div>This better say 21: <SuperComponent magicNumber={21}/> </div>
 
 
       {/* send in a prop called "product", give it the variable product*/}
-      <div>This better say "ajax": <SuperComponent /> </div>
+      <div>This better say "ajax": <SuperComponent product={product}/> </div>
       {/* send in a prop called "product", give it an object with a key of "name" and a value "pepsi"*/}
       <div>This better say "pepsi": <SuperComponent /> </div>
       {/* send in a prop called "product", give it an object with a key of "name" and a value "nike"*/}
